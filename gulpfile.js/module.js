0a021fe0b5073475bbd7f59a2f4ee99eb0ff1230
sunsh <sunsh8518@firstshare.cn>
2020-09-29 11:36:14 +0800

    module: webpack

--- gulpfile.js/module.js
+++ gulpfile.js/module.js
@@ -1,4 +1,6 @@
 // https://github.com/Seasons123/ADReact/issues/32
+// https://github.com/mqyqingfeng/Blog/issues/108
+
 /* 模块化：解决某一复杂问题时，依照分类思维把问题系统性的分解以处理之。
   模块化系统必须的能力：1：定义封装的模块。2：定义新模块对其他模块的依赖。3：可以对其他模块的引入支持。
 
@@ -60,3 +62,80 @@
 })
 
 /* rollup可以把es2015编译成amd,commonjs, umd */
+
+
+/* 
+commonjs（类似seajs执行结果，require时加载模块，加载完执行）与es6（类似requirejs结果：加载所有-》执行）差异：
+  1：CommonJS 模块是运行时加载，ES6 模块是编译时输出接口
+    commonjs加载的是一个对象（module.exports), 该对象只有在脚本执行完才会产生。
+    es6模块不是对象，对外接口只是一种静态定义，在代码静态解析阶段就会产生。
+  2：CommonJS 模块输出的是一个值的拷贝，ES6 模块输出的是值的引用
+    输出值拷贝一旦输出模块内部变化不会影响该值（对象除外：地址的拷贝），会缓存变量值。
+    es6: JS 引擎对脚本静态分析的时候，遇到模块加载命令 import，就会生成一个只读引用。等到脚本真正执行时，再根据这个只读引用，到被加载的那个模块里面去取值。动态引用，并且不会缓存值，模块里面的变量绑定其所在的模块
+*/
+
+
+/* webpack */
+// babel 与 webpack(browserify往全局注入require和module.exports，还把引用的模块进行打包，浏览器加载完就能用)
+// es6经babel编译后变成类似commonjs规范代码，所以要在浏览器模拟module,exports,require。
+// webpack会把模块包一层
+
+// 直接执行查看结果：异步组件使用jsonp原理动态插入script标签，查看webpack编译后的源码
+void function(modules){ // 加void把后面函数声明识别为一个表达式
+
+  var installedModules = {}; // 缓存已经加载的模块
+  function require(moduleName) {
+    if (installedModules[moduleName]) {
+      return installedModules[moduleName].exports;
+    }
+
+    var module = installedModules[moduleName] = {
+      exports: {}
+    }
+
+    modules[moduleName](module, module.exports, require); // exports = module.exports
+
+    return module.exports;
+  }
+
+  return require('main'); // 加载主模块
+}({
+  "main": function(module, exports, require) {
+
+      var addModule = require("./add");
+      console.log(addModule.add(1, 1))
+
+      var squareModule = require("./square");
+      console.log(squareModule.square(3));
+
+  },
+  "./add": function(module, exports, require) {
+      console.log('加载了 add 模块');
+
+      module.exports = {
+          add: function(x, y) {
+              return x + y;
+          }
+      };
+  },
+  "./square": function(module, exports, require) {
+      console.log('加载了 square 模块');
+
+      var multiply = require("./multiply");
+      module.exports = {
+          square: function(num) {
+              return multiply.multiply(num, num);
+          }
+      };
+  },
+
+  "./multiply": function(module, exports, require) {
+      console.log('加载了 multiply 模块');
+
+      module.exports = {
+          multiply: function(x, y) {
+              return x * y;
+          }
+      };
+  }
+})
